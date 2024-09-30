deef06c7514c676ee995285c5c15308a019637e9
JensKristianRuben <Jenskristian@gmail.dk>
2024-09-30 14:07:30 +0200

    push test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,15 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("weird");
         System.out.println("Mega weird");
+
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println("a");
+            for (int j = 0; j <20 ; j++) {
+                System.out.println("b");
+
+            }
+
+        }
     }
 }
